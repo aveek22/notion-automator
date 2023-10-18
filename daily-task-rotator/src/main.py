a79368a8bb78fdfe64bc9854c6d2a4c06fe9faf9
Aveek Das <aveek.das@dpgmedia.be>
2023-10-18 23:49:20 +0200

    Update app

--- daily-task-rotator/src/main.py
+++ daily-task-rotator/src/main.py
@@ -1,10 +1,11 @@
 import os
 from dotenv import load_dotenv
-from rotator.daily_task_rotator import get_database
+from rotator.daily_task_rotator import get_database, get_task_due_today
 
 load_dotenv()
 database_id = os.environ.get("DB_TASKS")
 token = os.environ.get("NOTION_SECRET")
 
 if __name__ == "__main__":
-    get_database(database_id, token)
+    # get_database(database_id, token)
+    get_task_due_today(database_id, token)
--- daily-task-rotator/src/rotator/daily_task_rotator.py
+++ daily-task-rotator/src/rotator/daily_task_rotator.py
@@ -16,6 +16,31 @@ def get_database(database_id: str, token: str):
         response_json = json.loads(response.text)
         print(f"Database name: {response_json['title'][0]['text']['content']}")
     else:
-        print(response.status_code)
+        print(f"Error: {response.status_code}")
         print(response.text)
 
+
+def get_task_due_today(database_id: str, token: str):
+    url = f"https://api.notion.com/v1/databases/{database_id}/query"
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Notion-Version': '2022-06-28',
+        'Content-Type': 'application/json'
+    }
+    data = {
+        "filter": {
+            "property": "Due Date",
+            "date": {
+                "equals": "2023-10-18"
+            }
+        }
+    }
+    print(f"Using token: {token} and database: {database_id}")
+    response = requests.post(headers=headers, url=url, json=data)
+
+    if response.status_code == 200:
+        response_json = json.loads(response.text)
+        print(f"Task Name: {response_json['results'][0]['properties']['Name']['title'][0]['text']['content']}")
+    else:
+        print(f"Error: {response.status_code}")
+        print(response.text)
