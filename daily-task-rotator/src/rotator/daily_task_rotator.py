21c4cb7a19c42865251677dbb972437727422230
Aveek Das <aveek.das@dpgmedia.be>
2023-10-22 22:54:06 +0200

    Update due date

--- daily-task-rotator/src/rotator/daily_task_rotator.py
+++ daily-task-rotator/src/rotator/daily_task_rotator.py
@@ -1,5 +1,8 @@
+from datetime import date
 import requests
 import json
+
+from rotator.util import get_yesterday, get_today
 
 
 def get_database(database_id: str, token: str):
@@ -44,3 +47,58 @@ def get_task_due_today(database_id: str, token: str):
     else:
         print(f"Error: {response.status_code}")
         print(response.text)
+
+
+def get_task_due_yesterday(database_id: str, token: str):
+    url = f"https://api.notion.com/v1/databases/{database_id}/query"
+    due_date = get_yesterday()
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Notion-Version': '2022-06-28',
+        'Content-Type': 'application/json'
+    }
+    data = {
+        "filter": {
+            "property": "Due Date",
+            "date": {
+                "equals": f"{due_date}"
+            }
+        }
+    }
+    print(f"Using token: {token} and database: {database_id}")
+    response = requests.post(headers=headers, url=url, json=data)
+
+    if response.status_code == 200:
+        response_json = json.loads(response.text)
+        # print(f"Task Name: {response_json['results'][0]['properties']['Name']['title'][0]['text']['content']}")
+        print(f"Task Name: {response_json}")
+    else:
+        print(f"Error: {response.status_code}")
+        print(response.text)
+
+
+def udpate_task_due_date(task_id: str, token: str, due_date: date):
+    url = f"https://api.notion.com/v1/pages/{task_id}"
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Notion-Version': '2022-06-28',
+        'Content-Type': 'application/json'
+    }
+    data = {
+        "properties": {
+            "Due Date": {
+                "date": {"start": f"{due_date}"}
+            }
+        }
+    }
+    print(f"Using token: {token} and page: {task_id}")
+    response = requests.patch(headers=headers, url=url, json=data)
+
+    if response.status_code == 200:
+        response_json = json.loads(response.text)
+        # print(f"Task Name: {response_json['results'][0]['properties']['Name']['title'][0]['text']['content']}")
+        print(f"Task Name: {response_json}")
+    else:
+        print(f"Error: {response.status_code}")
+        print(response.text)
+
