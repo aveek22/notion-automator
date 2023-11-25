277674c387794757e20acd2065512c78b47c9cc5
Aveek Das <aveek.das@dpgmedia.be>
2023-11-25 12:23:41 +0100

    Add TODOs

--- daily-task-rotator/src/main.py
+++ daily-task-rotator/src/main.py
@@ -4,6 +4,8 @@ import rotator.config as config
 
 # TODO: Implement logger
 # TODO: Send update to slack / email for due today
+# TODO: Implement code to read secrets from AWS Secret Manager
+# TODO: Figure out the entire build process and CICD for Lambda
 
 def main():
     rotator = DailyTaskRotator(config)
